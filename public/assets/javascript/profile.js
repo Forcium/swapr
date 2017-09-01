bb7cca5aeae94607f759bf43f7e5d07ffbef29a4
nanette julius <nanettejulius@nanettes-MacBook-Pro-3.local>
2017-09-01 15:49:37 -0700

    9.1

--- public/assets/javascript/profile.js
+++ public/assets/javascript/profile.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $('#pendingSwaps').hide();
+  $('#listOfItems').hide();
+  $('#changeProfile').hide();
+  $('#stuffUwant').hide();
+
+
 
   //modals
   $('.modal').modal({
-      dismissible: true, // Modal can be dismissed by clicking outside of the modal
-    });
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+  });
   // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
@@ -12,28 +18,61 @@ $(document).ready(function(){
   //upload images
 
   var options = {
-        beforeSubmit: showRequest, // pre-submit callback
-        success: showResponse // post-submit callback
-      };
+    beforeSubmit: showRequest, // pre-submit callback
+    success: showResponse // post-submit callback
+  };
 
-     // bind to the form's submit event
-      $('#listing').submit(function() {
-        $(this).ajaxSubmit(options);
-        // always return false to prevent standard browser submit and page navigation
-        return false;
-      });
+  // bind to the form's submit event
+  $('#listing').submit(function() {
+    $(this).ajaxSubmit(options);
+    // always return false to prevent standard browser submit and page navigation
+    return false;
+  });
 
-      //write to page
+//nav bar profile onclick functions=================
 
-    });
+  //list of items
+  $('#urStuff').on("click", function() {
+    $('#listOfItems').show();
+    $('#changeProfile').hide();
+    $('#pendingSwaps').hide();
+    $('#stuffUwant').hide();    
+    $('.button-collapse').sideNav('hide');
+  })
+  //offers made
+  $('#offer').on("click", function() {
+    $('#stuffUwant').show();
+    $('#listOfItems').hide();
+    $('#changeProfile').hide();
+    $('#pendingSwaps').hide();    
+    $('.button-collapse').sideNav('hide');
+  })
+  //pending swaps
+  $('#pend').on("click", function() {
+    $('#listOfItems').hide();
+    $('#changeProfile').hide();
+    $('#stuffUwant').hide();
+    $('#pendingSwaps').show();    
+    $('.button-collapse').sideNav('hide');
+  })
+  //update profile
+  $('#update').on("click", function() {
+    $('#changeProfile').show();
+    $('#pendingSwaps').hide();
+    $('#listOfItems').hide();
+    $('#stuffUwant').hide();
+    $('.button-collapse').sideNav('hide');
+  })
 
-   // pre-submit callback
-    function showRequest(formData, jqForm, options) {
-      alert('Uploading is starting.');
-      return true;
-    }
+}); //doc ready
 
-   // post-submit callback
-    function showResponse(responseText, statusText, xhr, $form) {
-      alert('status: ' + statusText + '\n\nresponseText: \n' + responseText);
-    }
+// pre-submit callback
+function showRequest(formData, jqForm, options) {
+  alert('Uploading is starting.');
+  return true;
+}
+
+// post-submit callback
+function showResponse(responseText, statusText, xhr, $form) {
+  alert('status: ' + statusText + '\n\nresponseText: \n' + responseText);
+}
