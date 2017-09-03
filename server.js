78d0ba9d0152f39d6f591241f313f9c643a009f6
Elton Hsu <eltonhsu@MacBook-Pro-3.local>
2017-09-03 01:22:55 -0700

    avatar upload

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -23,6 +24,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
 app.use(express.static("public"));
+
 
 // Routes
 // =============================================================
