5f3f9746b75b048e5b18c896f14a3ebac5791c58
Elton Hsu <eltonhsu@MacBook-Pro-3.local>
2017-09-04 13:58:21 -0700

    monday1

--- public/assets/javascript/listing.js
+++ public/assets/javascript/listing.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+
+      $('#carousel1').attr('src', '/assets/userUpload/1.png');
+      $('#carousel2').attr('src', '/assets/userUpload/2.png');
+      $('#carousel3').attr('src', '/assets/userUpload/3.png');
+
+      
       $('.carousel').carousel();
-    });
+
+
+
+
+
+});
