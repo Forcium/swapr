ef8619512c7bbb179a2588bdfd50647c524007b5
Elton Hsu <eltonhsu@MacBook-Pro-3.local>
2017-09-07 10:43:58 -0700

    chat box

--- public/assets/javascript/chat.js
+++ public/assets/javascript/chat.js
@@ -1,53 +1,64 @@
+$.post("/", {
+  token: window.localStorage.getItem("token")
+}).then(function(data) {
 
-var config = {
-    apiKey: "AIzaSyAvfQNHxo8df0lNiPlMWFgu95tBl8rd8Eg",
-    authDomain: "class-test-829c1.firebaseapp.com",
-    databaseURL: "https://class-test-829c1.firebaseio.com",
-    projectId: "class-test-829c1",
-    storageBucket: "class-test-829c1.appspot.com",
-    messagingSenderId: "38207736538"
-  };
+  if (!data) {
 
-firebase.initializeApp(config);
+    window.localStorage.clear();
+    window.location.href = "/";
 
-var userData = firebase.database();
-$("#add-msg-btn").on("click", function() {
+  } else {
 
-  var user = $("#user-name-input").val().trim();
-  var messages = $("#message-input").val().trim();
 
-  var newTrain = {
+    var config = {
+      apiKey: "AIzaSyAvfQNHxo8df0lNiPlMWFgu95tBl8rd8Eg",
+      authDomain: "class-test-829c1.firebaseapp.com",
+      databaseURL: "https://class-test-829c1.firebaseio.com",
+      projectId: "class-test-829c1",
+      storageBucket: "class-test-829c1.appspot.com",
+      messagingSenderId: "38207736538"
+    };
 
-    name: user,
-    message: messages,
-  };
+    firebase.initializeApp(config);
 
-  userData.ref().push(newTrain);
 
-  console.log(newTrain.name);
-  console.log(newTrain.message);
+    var userData = firebase.database();
 
-  alert("Message sent");
 
-  $("#user-name-input").val("");
-  $("#message-input").val("");
-  return false;
+    $("#add-msg-btn").on("click", function() {
+
+
+
+      var user = data.avatar;
+      var messages = $("#message-input").val().trim();
+
+      var newTrain = {
+
+        name: user,
+        message: messages,
+      };
+
+      userData.ref().push(newTrain);
+
+
+      $("#message-input").val("");
+      return false;
+
+    });
+
+    userData.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+      console.log(childSnapshot.val());
+
+      var userName = childSnapshot.val().name;
+      var userMessage = childSnapshot.val().message;
+
+
+      $("#msg-table > tbody").append("<tr><td><img id='avatarImg' class='circle' src='" + userName + "'></td><td><div class='chatMessage'>" + userMessage + "</div></td></tr>");
+
+
+    });
+
+  }
 
 });
-
-userData.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-  console.log(childSnapshot.val());
-
-  var userName = childSnapshot.val().name;
-  var userMessage = childSnapshot.val().message;
-
-
-  console.log(userName);
-  console.log(userMessage);
-  
-
-  $("#msg-table > tbody").append("<tr><td>" + userName + "</td><td>" + userMessage + "</td></tr>");
-
-});
-
