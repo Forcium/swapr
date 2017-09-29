d34a2d51358754e4b204065d085ced552d8f3ef5
J373my1 <Jeremy.Awad@gmail.com>
2017-09-28 20:17:49 -0700

    chat Features

--- public/assets/javascript/chat.js
+++ public/assets/javascript/chat.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+var roomID;
 
 $.post("/", {
   token: window.localStorage.getItem("token")
@@ -17,7 +18,9 @@ $.post("/", {
       userID: uid
     }).then(function(dbReponse){
       console.log(dbReponse);
-      $("#transNumber").html("Transaction #110352224925" + dbReponse.id)
+      var transId = "Transaction #110352224925" + dbReponse.id;
+      roomID = "110352224925" + dbReponse.id;
+      $("#transNumber").html(transId);
 
       var yourID;
       var yourItem;
@@ -44,8 +47,6 @@ $.post("/", {
         $('#yourItem').html(response.item_description);
       });
       $.get("/communicate/otherItem/" + otherItem, function(response2){
-        $('#otherItemC').attr("src",response2.item_img1);
-        $('#otherItem').html(response2.item_description);
       });
 
     var config = {
@@ -60,59 +61,42 @@ $.post("/", {
     firebase.initializeApp(config);
 
     var userData = firebase.database();
-
+    var postId;
         $("#add-msg-btn").on("click", function() {
-
+          console.log(roomID);
           var user = data.avatar;
           var messages = $("#message-input").val().trim();
 
-          var newTrain = {
-            "transId" : {
-                      name: user,
-                      message: messages
-                }
+          var newMsg = {
+                name: user,
+                message: messages
           };
 
-          var newPostRef = userData.ref().push(newTrain);
-          var postId = newPostRef.key;
-          console.log(newPostRef);
+          var newPostRef = userData.ref().child(roomID).push(newMsg);
+          postId = newPostRef.key;
 
           $("#message-input").val("");
           return false;
 
         });
 
-      userData.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-        var userName = childSnapshot.val().name;
-        var userMessage = childSnapshot.val().message;
-
-        $("#msg-table > tbody").append("<tr><td><img id='avatarImg' class='circle' src='" + userName + "'></td><td><div class='chatMessage'>" + userMessage + "</div></td></tr>");
+      userData.ref(roomID).on("child_added", function(childSnapshot, prevChildKey) {
+        console.log(roomID);
+        console.log("------------------------");
+        var route2 = childSnapshot.val();
+        $("#msg-table > tbody").append("<tr><td><img id='avatarImg' class='circle' src='" +
+        route2.name + "'></td><td><div class='chatMessage'>" + route2.message + "</div></td></tr>");
         updateScroll();
 
       });
-
     });
   }
 });
 
-//~~~~~~~~~~~~~~~~~~navbar search function
-
-$(".topInputBar").on("keydown", function(event){
-
-  if (event.which == 13){
-    var text = $("#search").val();
-    event.preventDefault();
-    window.location.href="/search/"+text;
-  }
-});
-
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   function updateScroll(){
       var element = document.getElementById("Convos");
       element.scrollTop = element.scrollHeight;
   }
 
-})
+});
